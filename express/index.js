ef381e8dc41c4f8b5050e9a8d3f7d9b650e16f10
Bibhudutta panda <bibhuduttapanda7@gmail.com>
2025-06-24 20:07:55 +0530

    easy routing

--- express/index.js
+++ express/index.js
@@ -3,9 +3,23 @@ const express= require('express');
 const app= express();
 
 app.get('/', (req,res)=>{
-    res.end("Hello world!");
+    res.send("Hello world!");
 });
+
+//dynamic parameter 
+app.get('/user/:username', (req,res)=>{
+    const username= req.params.username;
+    res.send(`Welcome ${username}`);
+});
+
+//query is used for searching
+// ..../search?keyword=input
+
+app.get('/search', (req,res)=>{
+    const keyword= req.query.keyword;
+    res.send(`Searching for ${keyword}`);
+})
 
 app.listen(3000, ()=>{
     console.log("Server is running at address http://localhost:3000");
-});
+}); 
