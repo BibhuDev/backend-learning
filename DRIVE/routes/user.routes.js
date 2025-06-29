c61d25c78177b2608d901d211be2280f6ed2f216
Bibhudutta panda <bibhuduttapanda7@gmail.com>
2025-06-29 10:31:50 +0530

    register page complete

--- DRIVE/routes/user.routes.js
+++ DRIVE/routes/user.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model');
+const user = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 router.get('/register', (req,res)=>{
     res.render("register");
@@ -10,13 +13,22 @@ router.post('/register',
     body('email').trim().isEmail({min:10}),
     body('password').trim().isLength({min:5}),
     body('username').trim().isLength({min:3}),
-    (req,res)=>{
+    async (req,res)=>{
 
         const errors = validationResult(req);
         console.log(errors);
 
-    console.log(req.body);
-    console.log("user registered");
+    const {email, username, password} = req.body;
+
+    const hashPass = await bcrypt.hash(password, 10);
+
+    const newUser = await userModel.create({
+        email,
+        username,
+        password: hashPass
+    })
+
+    res.json(newUser);
 })
 
-module.exports= router;
+module.exports= router; 
